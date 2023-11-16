5ef461ad89b529cd08bbe2afa05452efcae188de
akumukernel <marcelo.fuenzalida@protonmail.com>
2023-11-16 17:23:13 -0300

    Fourth commit

---
+++ Hadoop/proyecto/buscador.py
@@ -0,0 +1,58 @@
+import psycopg2
+import re
+import json
+
+connection = psycopg2.connect(  user = "postgres",
+                                password = "postgres",
+                                host = "db",
+                                port = "5432",
+                                database = "proyecto")
+
+try:
+    cursor = connection.cursor()
+    print("Registrando elementos a la base de datos...")
+    with open('outhadoop/part-00000','r') as archivo:
+     next(archivo)
+     for linea in archivo:
+         datos = re.findall(r'\b\w+\b|\([^)]*\)', linea)
+
+         letra = datos[0]
+         pares = [tuple(map(int, par.strip('()').split(' '))) for par in datos[1:]]
+         for n, m in pares:
+             cursor.execute("INSERT INTO registros (palabra, documento, repeticiones) VALUES (%s, %s, %s) ON CONFLICT (palabra, documento) DO NOTHING", (letra, n, m))
+         
+         connection.commit()
+    
+    while True:
+         print("\nMenú")
+         print("1. Buscar palabra")
+         print("2. Salir")
+
+         opcion = input("Seleccione una opción: ")
+         if opcion == "1":
+            palabra = input("Inserte palabra: ")
+
+            query = f"SELECT documento, repeticiones, url FROM paginas INNER JOIN registros ON paginas.id = registros.documento WHERE palabra = '{palabra}'"
+            cursor.execute(query)
+
+            resultados = cursor.fetchall()
+            resultados_json = []
+            column_names = [desc[0] for desc in cursor.description]
+
+            for fila in resultados:
+                fila_json = dict(zip(column_names, fila))
+                resultados_json.append(fila_json)
+            
+            print(json.dumps(resultados_json, indent=2))
+         elif opcion == "2":
+            break
+         else:
+            print("Comando Incorrecto")
+
+except (Exception, psycopg2.Error) as error :
+    print ("Error while connecting to PostgreSQL", error)
+finally:
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
--- Hadoop/proyecto/mapper.py
+++ Hadoop/proyecto/mapper.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
 # -*-coding:utf-8 -*
 import sys
+import psycopg2
+
+connection = psycopg2.connect(  user = "postgres",
+                                password = "postgres",
+                                host = "db",
+                                port = "5432",
+                                database = "proyecto")
 
 for line in sys.stdin:
     docs = line.lower()
     arr = []
-
-    # Replace punctuation and numerical values with empty strings
-    for char in [",", ".", '"', "'", "(", ")", "\\", ";", ":", "$1", "$", "&", "="]:
-        docs = docs.replace(char, '')
+    i = 0
 
     # Split the document into name and document parts
     name, docs = docs.split('<splittername>')
     name, url = name.split()
+
+    # Replace punctuation and numerical values with empty strings
+    for char in [",", ".", '"', "'", "(", ")", "\\", ";", ":", "$1", "$", "&", "="]:
+        docs = docs.replace(char, '')
 
     # Process each word in the document
     for word in docs.split():
@@ -26,4 +34,17 @@ for line in sys.stdin:
 
     # Sort and print the processed words
     for i in sorted(arr):
-        print(i)
+      print(i)
+    try:
+     cursor = connection.cursor()
+     for i in range(30):
+      cursor.execute("INSERT INTO paginas (id, url) VALUES (%s, %s) ON CONFLICT (id) DO NOTHING", (name, url))
+      connection.commit()
+
+    except (Exception, psycopg2.Error) as error :
+     print ("Error while connecting to PostgreSQL", error)
+    finally:
+         if(connection):
+             cursor.close()
+             connection.close()
+      
